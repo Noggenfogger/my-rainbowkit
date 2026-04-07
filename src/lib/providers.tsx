d7369951a9df3c3c02259d452a1a8f4b7d9d59d5
Noggenfogger <megathron@live.com>
2026-04-07 17:59:54 +0800

    本地化

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
+  type Locale,
   RainbowKitProvider,
   lightTheme,
   darkTheme,
@@ -10,6 +11,7 @@ import {
 import { type ReactNode, useState } from "react";
 import { type State, WagmiProvider } from "wagmi";
 import { getConfig } from "@/src/lib/wagmi";
+import { useLocale } from "next-intl";
 
 export function Providers(props: {
   children: ReactNode;
@@ -17,6 +19,7 @@ export function Providers(props: {
 }) {
   const [config] = useState(() => getConfig()); // 客户端惰性初始化（仅执行1次）
   const [queryClient] = useState(() => new QueryClient());
+  // const locale = useLocale() as Locale; // 从 next-intl 拿当前语言
 
   return (
     <WagmiProvider config={config} initialState={props.initialState}>
@@ -39,6 +42,8 @@ export function Providers(props: {
           // modalSize="compact"    //紧凑模态框
           // initialChain={1}       //初始链配置：用链ID
           // initialChain={mainnet} //初始链配置：用链对象
+          // locale="zh-CN"         //rainbowkit本地化
+          // locale={locale}        //rainbowkit本地化：这个网站多语言
         >
           {props.children}
         </RainbowKitProvider>
