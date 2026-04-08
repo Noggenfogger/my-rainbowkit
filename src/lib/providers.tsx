649bffdf26061766cb6c89e0a2191fe083e2406a
Noggenfogger <megathron@live.com>
2026-04-09 00:42:27 +0800

    TODO 认证/better-auth/postgreSQL/prisma

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useState } from "react";
+import { type State, WagmiProvider } from "wagmi";
+import { getConfig } from "@/src/lib/wagmi";
+import { useLocale } from "next-intl";
 import {
   type Locale,
   RainbowKitProvider,
@@ -8,10 +12,6 @@ import {
   darkTheme,
   midnightTheme,
 } from "@rainbow-me/rainbowkit";
-import { type ReactNode, useState } from "react";
-import { type State, WagmiProvider } from "wagmi";
-import { getConfig } from "@/src/lib/wagmi";
-import { useLocale } from "next-intl";
 
 export function Providers(props: {
   children: ReactNode;
@@ -43,7 +43,7 @@ export function Providers(props: {
           // initialChain={1}       //初始链配置：用链ID
           // initialChain={mainnet} //初始链配置：用链对象
           // locale="zh-CN"         //rainbowkit本地化
-          // locale={locale}        //rainbowkit本地化：这个网站多语言
+          // locale={locale}        //rainbowkit本地化：整个网站多语言
         >
           {props.children}
         </RainbowKitProvider>
